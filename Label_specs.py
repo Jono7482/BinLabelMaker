5b9d9ed8f3d51853dc51083face1149350e9de5d
jono <JonoLaguna@gmail.com>
2023-03-07 12:40:05 -0800

    switched to pillow about to remove tk labels

--- Label_specs.py
+++ Label_specs.py
@@ -2,7 +2,7 @@ font_type = 'Raleway'
 default_string = 'MC-1...2-A...B'
 logo_path = 'data/Laguna Logo_2022 color.png'
 label_formats = {'1 8X10': {
-                    'lpp': 1,
+                    'lpp': (1, 1),
                     'scale': 0.70,
                     'canvas_height': 1200,
                     'canvas_width': 850,
@@ -20,7 +20,7 @@ label_formats = {'1 8X10': {
                     'font_size': 60,
                 },
                 '2 4X6': {
-                    'lpp': 2,
+                    'lpp': (2, 1),
                     'scale': 1,
                     'canvas_height': 800,
                     'canvas_width': 1200,
@@ -38,7 +38,7 @@ label_formats = {'1 8X10': {
                     'font_size': 60,
                 },
                 '4 6X4': {
-                    'lpp': 4,
+                    'lpp': (1, 4),
                     'scale': 0.60,
                     'canvas_height': 1400,
                     'canvas_width': 800,
@@ -54,5 +54,60 @@ label_formats = {'1 8X10': {
                     'logo_y': 160,
                     'font_width': 9,
                     'font_size': 28,
+                },
+                }
+label_formats_pil = {'1 8X10': {
+                    'lpp': (1, 1),
+                    'scale': 0.70,
+                    'canvas_height': 1100,
+                    'canvas_width': 850,
+                    'qr_size': 24,
+                    'x_shift': 0,
+                    'y_shift': 0,
+                    'qr_x': 25,
+                    'qr_y': 25,
+                    'text_x': 425,  # canvas width / 2
+                    'text_y': 845,
+                    'logo': 230,
+                    'logo_x': 280,  # canvas width / 2 - logo / 2
+                    'logo_y': 870,
+                    'font_width': 21,
+                    'font_size': 100,
+                },
+                '2 4X6': {
+                    'lpp': (2, 1),
+                    'scale': 1,
+                    'canvas_height': 850,
+                    'canvas_width': 1100,
+                    'qr_size': 15,
+                    'x_shift': 590,
+                    'y_shift': 0,
+                    'qr_x': 20,
+                    'qr_y': 20,
+                    'text_x': 275,  # canvas width / 4
+                    'text_y': 570,
+                    'logo': 160,
+                    'logo_x': 195,  # canvas width / 4 - logo / 2
+                    'logo_y': 620,
+                    'font_width': 21,
+                    'font_size': 65,
+                },
+                '4 6X4': {
+                    'lpp': (1, 4),
+                    'scale': 0.60,
+                    'canvas_height': 1400,
+                    'canvas_width': 800,
+                    'qr_size': 8,
+                    'x_shift': 0,
+                    'y_shift': 200,
+                    'qr_x': 50,
+                    'qr_y': 50,
+                    'text_x': 528,  # canvas width * .66
+                    'text_y': 100,
+                    'logo': 160,
+                    'logo_x': 448,  # canvas width * .66 - logo size / 2
+                    'logo_y': 160,
+                    'font_width': 9,
+                    'font_size': 50,
                 },
                 }
